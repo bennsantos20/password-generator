9e3494e2031a4e691f7b52207aa51cd834d722ce
Benjamin <bennsantos20@gmail.com>
2021-03-20 22:47:01 -0400

    added javascript to enable functions of password generator

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # password-generator
+
+# Description
+This portfolio is containing HTML, CSS, and Javascript files that will allow the user to engage in functions to create their own respective passwords by being able to choose how long, what characters, numbers, etc.
+
+![Portfolio Site](./assets/screenshot.png)
+
+[password-generator]
--- script.js
+++ script.js
@@ -1,14 +1,123 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
 
+
+function generatePassword() {
+  var userAnswer = prompt("How many characters would you like?") 
+
+  if (!isValidLengthOfPassword(userAnswer)){
+      var message = 'Please provide an integer between 8-128';
+      console.log(message);
+      alert(message);
+      return;
+  }
+
+  // generate password
+  var lengthOfPassword = parseInt(userAnswer);
+
+  var isNumberRequired = confirm("Will this password contain numbers?");
+  var isSpecRequired = confirm("Will this password contain special characters?");
+  var isUpperCaseRequired = confirm("Will this contain uppercase letters?");
+  var isLowerCaseRequired = confirm("Will this contain lowercase letters?");
+
+  if (!(isNumberRequired || isSpecRequired || isUpperCaseRequired || isLowerCaseRequired)) {
+     alert('need to require at least one type of character');
+     return;
+  }
+
+  var password = ""
+
+  var countOfCharTypes = 0;
+
+  if (isNumberRequired ) {
+    countOfCharTypes ++ 
+  }
+
+  if (isSpecRequired) {
+    countOfCharTypes ++
+  }
+
+  if (isUpperCaseRequired) {
+    countOfCharTypes ++
+  }
+
+  if (isLowerCaseRequired) {
+    countOfCharTypes ++
+  }
+
+  for(var i = 0; i < Math.ceil(lengthOfPassword/countOfCharTypes); i++) {
+    if (isLowerCaseRequired) {
+      password += generateRandomLowerCaseLetter(); 
+    }
+    if (isUpperCaseRequired) {
+        password += generateRandomUpperCaseLetter(); 
+    }
+    if (isNumberRequired) {
+        password += generateRandomNumber();
+    }
+    if (isSpecRequired) {
+        password += generateRandomSpec();
+    }
+  }
+
+  password = password.substring(0,lengthOfPassword);
+  
+  console.log(password)
+  var passwordText  = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+function isValidLengthOfPassword(userAnswer) {
+  var res = true;
+  if (isNaN(userAnswer)) {
+      console.log("Answer is not a number. Please provide an integer between 8-128.");
+      res = false;
+  }
+  var int = parseInt(userAnswer)
+  if (int < 8 || int > 128) {
+      console.log("Number is not within range. Please provide an integer between 8-128");
+      res = false;
+  }
+
+  return res;   
+}
+
+function generateRandomLowerCaseLetter() {
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];    
+  var arraySize = lowerCase.length;
+  var randomInt = getRandomInt(arraySize);
+  var randomLowerCase = lowerCase[randomInt]
+  return randomLowerCase
+}
+
+function generateRandomUpperCaseLetter() {
+  var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var arraySize = caps.length;
+  var randomInt = getRandomInt(arraySize);
+  var randomUpperCase = caps[randomInt]
+  return randomUpperCase
+}
+
+function generateRandomNumber() {
+  var int = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var arraySize = int.length;
+  var randomInt = getRandomInt(arraySize)
+  var randomNumber = int[randomInt]
+  return randomNumber
+}
+
+function generateRandomSpec () {
+  var spec = ["!", "@", "#", "$", "%", "&"]
+  var arraySize = spec.length;
+  var randomInt = getRandomInt(arraySize);
+  var randomSpec = spec[randomInt]
+  return randomSpec
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
